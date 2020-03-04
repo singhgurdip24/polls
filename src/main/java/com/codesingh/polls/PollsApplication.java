dbee506b3ad6ef9cbcaf19c406f9d814243b96c0
Gurdip Singh <singh.gurdip24@gmail.com>
2020-03-03 23:35:35 -0500

    First working model, added user classes

--- src/main/java/com/codesingh/polls/PollsApplication.java
+++ src/main/java/com/codesingh/polls/PollsApplication.java
@@ -2,9 +2,24 @@ package com.codesingh.polls;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+		PollsApplication.class,
+		//Jsr310 converter is required to convert all Java 8 date/time fields to SQL types to persist them in DB
+		Jsr310JpaConverters.class
+})
 public class PollsApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PollsApplication.class, args);
---
+++ src/main/java/com/codesingh/polls/model/User.java
@@ -0,0 +1,106 @@
+package com.codesingh.polls.model;
+
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+
+@Entity
+@Table(name = "users", uniqueConstraints =  {
+        @UniqueConstraint(columnNames = {
+                "username"
+        }),
+        @UniqueConstraint(columnNames = {
+                "email"
+        })
+})
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 40)
+    private String name;
+
+    @NotBlank
+    @Size(max = 15)
+    private String username;
+
+    @NotBlank
+    @NaturalId
+    @Size(max = 40)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size (max = 100)
+    private String password;
+
+    private Set<Role> roles =new HashSet<>();
+
+    public User() {
+
+    }
+
+    public User(@NotBlank @Size(max = 40) String name, @NotBlank @Size(max = 15) String username, @NotBlank @Size(max = 40) @Email String email, @NotBlank @Size(max = 100) String password) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
