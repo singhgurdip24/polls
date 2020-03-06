1be34ce1bba027229f4365b27b8b6a0682da7316
Gurdip Singh <singh.gurdip24@gmail.com>
2020-03-05 22:55:10 -0500

    fixed missing error

--- src/main/java/com/codesingh/polls/model/audit/DateAudit.java
+++ src/main/java/com/codesingh/polls/model/audit/DateAudit.java
@@ -1,14 +1,14 @@
 package com.codesingh.polls.model.audit;
 
-        import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-        import org.springframework.data.annotation.CreatedDate;
-        import org.springframework.data.annotation.LastModifiedDate;
-        import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-        import javax.persistence.Column;
-        import javax.persistence.EntityListeners;
-        import javax.persistence.MappedSuperclass;
-        import java.io.Serializable;
-        import java.time.Instant;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.Instant;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
